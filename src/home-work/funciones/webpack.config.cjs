4cc08e54b3807096f3bf51bfd8ec46d5a4aedd56
ASuárezTabares <91555979+AdrianSuarezTabares@users.noreply.github.com>
2024-03-18 19:51:34 +0000

    Cambiada la estructura del directorio

--- src/home-work/funciones/webpack.config.cjs
+++ src/home-work/funciones/webpack.config.cjs
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development", // Add the development mode
-  entry: "./main.ts",
+  entry: "./app/main.ts",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
